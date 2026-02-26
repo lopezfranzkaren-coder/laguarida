1864e3353feb43f20b681c1a128c6d6d837f009b
lopezfranzkaren-coder <lopezfranz.karen@gmail.com>
2026-02-26 02:02:44 -0300

    Update app.py

--- app.py
+++ app.py
@@ -446,6 +446,9 @@ def get_cliente_pedidos(nombre):
     return jsonify(result)
 
 if __name__ == "__main__":
-    init_db(); app.run(debug=True, port=5000)
+    with app.app_context():
+        init_db()
+    app.run(debug=True, port=5000)
 
-init_db()
+with app.app_context():
+    init_db()
