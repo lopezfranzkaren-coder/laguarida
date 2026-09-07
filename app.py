62fcf933ce0a19a4344e0f8d55463b6126a75be3
lopezfranzkaren-coder <lopezfranz.karen@gmail.com>
2026-09-07 13:56:26 -0300

    Update app.py

--- app.py
+++ app.py
@@ -276,6 +276,7 @@ def add_producto():
             get_db().commit()
         return jsonify({"ok":True})
     except: return jsonify({"ok":False,"error":"Ya existe"}),400
+
 @app.route("/api/productos/<int:pid>", methods=["PUT"])
 def upd_producto(pid):
     d = request.json
